2e2841df09fad9e93fff3a9b8d45d9bbf1cf0dea
song <123>
2020-08-10 13:47:57 +0800

    2

--- src/main/java/GitText.java
+++ src/main/java/GitText.java
@@ -4,6 +4,8 @@ public class GitText {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helloGit");
+		System.out.println("¸üÐÂ1");
+		
 	}
 
 }
