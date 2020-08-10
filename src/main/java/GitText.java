3fa784e9b6a2f92cc524c1e8666b63d3394de0ff
songqinglin <55488254+songqinglin@users.noreply.github.com>
2020-08-10 16:17:58 +0800

    在线更新

--- src/main/java/GitText.java
+++ src/main/java/GitText.java
@@ -4,12 +4,13 @@ public class GitText {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helloGit");
-		System.out.println("更新1");
-		System.out.println("创建分支");
+		System.out.println("鏇存柊1");
+		System.out.println("鍒涘缓鍒嗘敮");
 
-		System.out.println("主干");
+		System.out.println("涓诲共");
 
-		System.out.println("分支添加");
+		System.out.println("鍒嗘敮娣诲姞");
+System.out.println("鍦ㄧ嚎娣诲姞");
 
 	}
 
