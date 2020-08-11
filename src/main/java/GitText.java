5e2e2e74ad9112bbd9c9af20a0e1d8a9bb632b9b
song <123>
2020-08-11 09:55:03 +0800

    第3次

--- src/main/java/GitText.java
+++ src/main/java/GitText.java
@@ -14,13 +14,9 @@ public class GitText {
 
 
 
-		System.out.println("更新1");
-		System.out.println("创建分支");
+		System.out.println("1");
+		System.out.println("3");
 
-		System.out.println("主干");
-
-		System.out.println("分支添加");
-                System.out.println("在线添加1");
 
 
 	}
