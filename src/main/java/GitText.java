5fd3d6b04ca21b2320352d21c80a2e268473d34e
song <123>
2020-08-11 09:39:52 +0800

    第二次更新。

--- src/main/java/GitText.java
+++ src/main/java/GitText.java
@@ -4,9 +4,17 @@ public class GitText {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helloGit");
+
 		System.out.println("更新1");
 		System.out.println("创建分支");
+
+		System.out.println("主干");
+
 		System.out.println("分支添加");
+		System.out.println("第二次提交之前更新");
+
+
+
 	}
 
 }
