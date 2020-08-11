0306c7fd33a1dc5814981a9ca2f4134f4042a6b0
song <123>
2020-08-11 09:58:05 +0800

    改

--- src/main/java/GitText.java
+++ src/main/java/GitText.java
@@ -3,7 +3,7 @@ public class GitText {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		System.out.println("helloGit");
 		System.out.println("更新1");
 		System.out.println("创建分支");
